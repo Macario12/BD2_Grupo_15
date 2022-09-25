623e03d0573248d0545b452f253466ff865dd294
macario12 <arielmacario.11@gmail.com>
2022-09-25 00:06:35 -0600

    actualizacion scripts

--- Proyecto 1/scriptDB.sql
+++ Proyecto 1/scriptDB.sql
@@ -1,11 +1,16 @@
-CREATE DATABASE Proyecto2Bases;
+CREATE DATABASE `Proyecto2Bases`;
 USE Proyecto2Bases;
+
+DROP database Proyecto2Bases;
+DROP TABLE LOG_HABITACION;
+DROP TABLE LOG_ACTIVIDAD;
+DROP TABLE HABITACION;
+DROP TABLE PACIENTE;
 
 CREATE TABLE HABITACION(
 	idHabitacion int primary key auto_increment,
     habitacion varchar(50)
 );
-DROP TABLE LOG_HABITACION;
 CREATE TABLE LOG_HABITACION(
 	idLogHabitacion int primary key auto_increment,
 	timestampx VARCHAR(100),
@@ -19,7 +24,6 @@ CREATE TABLE PACIENTE(
     edad INT,
     genero VARCHAR(20)
 );
-
 CREATE TABLE LOG_ACTIVIDAD(
 	id_log_actividad INT PRIMARY KEY auto_increment,
     timestampx VARCHAR(100),
@@ -28,31 +32,4 @@ CREATE TABLE LOG_ACTIVIDAD(
     HABITACION_idHabitacion INT,
     FOREIGN KEY (HABITACION_idHabitacion) REFERENCES HABITACION (idHabitacion) ON DELETE CASCADE,
     FOREIGN KEY (PACIENTE_idPaciente) REFERENCES PACIENTE (idPaciente) ON DELETE CASCADE 
-);
-
-
-CREATE TABLE TEMP_LOG_HABITACION(
-	idHabitacion int,
-    timestampx VARCHAR(100),
-    statusx VARCHAR(45),
-    Habitacion VARCHAR(250)
-    
-);
-
-CREATE TABLE TEMP_LOG_ACTIVIDAD(
-    timestampx VARCHAR(100),
-    actividad VARCHAR(500),
-    idHabitacion INT,
-    idPaciente INT,
-    Edad INT,
-    Genero VARCHAR(100),
-    Habitacion VARCHAR(250)
-	
-);
-
-CREATE TABLE TEMP_LOG_ACTIVIDAD2(
-    timestampx VARCHAR(100),
-    actividad VARCHAR(500),
-    idHabitacion INT,
-    idPaciente INT
 );
