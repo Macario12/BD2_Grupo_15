4b36d8be2282ad608d18b9c3645a8f8a4a66c464
GermanJosePazCordon <germanpc9@gmail.com>
2022-11-04 18:05:26 -0600

    log

--- Proyecto 2/Archivos de entrada - CSV/xd.py
+++ Proyecto 2/Archivos de entrada - CSV/xd.py
@@ -1,15 +1,15 @@
 class xd():
     def add(self, path):
         file = open(path, "r")
-        file2 = open("LogHabitaciones2.csv", "a")
+        file2 = open("LogActividades12.csv", "a")
         cont = 0
         for x in file:
-            if cont is 0:
-                file2.write("idLogHabitacion" + "," + x)
+            if cont == 0:
+                file2.write("idLogActividad" + "," + x)
             else:
                 file2.write(str(cont) + "," + x)  
             cont += 1
         file.close()
 
 a = xd()
-a.add("LogHabitaciones.csv")
+a.add("Logactividades1.csv")
